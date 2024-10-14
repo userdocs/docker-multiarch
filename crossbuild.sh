74268a6877b0eadb48dbbc4f535a2af837e1e697
userdocs <16525024+userdocs@users.noreply.github.com>
2024-10-14 18:29:21 +0100

    Update crossbuild.sh

--- crossbuild.sh
+++ crossbuild.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-# docker run -it -w /root -v ~/iperf3:/root ghcr.io/userdocs/qbt-musl-cross-make:aarch64-linux-musl
+github_repo="${1:-"https://github.com/esnet/iperf.git"}"
+github_branch="${2:"-master"}"
+crossbuild_target="${3:"-x86_64-linux-musl"}"
+
+printf '%s\n' "Building iperf3 for $crossbuild_target"
+printf '%s\n' "repo: $github_repo branch:$github_branch"
+
+# docker run -it -w /root -v ~/iperf3:/root ghcr.io/userdocs/qbt-musl-cross-make:aarch64-linux-musl /bin/bash crossbuild.sh
 
 apk update
 
@@ -21,7 +28,7 @@ tar -xzf "${openssl_dev}.apk" --strip-components=1 -C /root/local
 tar -xzf "${openssl_libs_static}.apk" --strip-components=1 -C /root/local
 
 rm -rf /root/iperf3
-git clone --no-tags --single-branch --branch "master" --shallow-submodules --recurse-submodules -j"$(nproc)" --depth 1 "https://github.com/esnet/iperf.git" /root/iperf3
+git clone --no-tags --single-branch --branch "${github_branch}" --shallow-submodules --recurse-submodules -j"$(nproc)" --depth 1 "${github_repo}" /root/iperf3
 cd /root/iperf3 || exit
 
 ./configure --with-openssl="/root/local" --disable-shared --enable-static-bin --prefix="/root/local"
